6f15912aaabe8a175bc0603f7462352421542175
Игорь Батталов <kakamakaka03@yandex.ru>
2024-03-29 13:07:36 +0700

    Добавлен черновой вариант 3 задачи C++

--- c_plusplus.cpp
+++ c_plusplus.cpp
@@ -3,6 +3,29 @@
 #include <set>
 #include <map>
 
+void zadanie3() {
+	std::cout << "Введите3" << std::endl;
+	int n = 0;
+	std::cin >> n;
+	std::vector<int> chisla;
+	for (int i = 0; i < n; i++) {
+		int n1 = 0;
+		std::cin >> n1;
+		chisla.push_back(n1);
+	}
+	int summa = 0;
+	for (int chislo : chisla) {
+		for (int cifra = 0; chislo > 0;chislo = chislo / 10) {
+			cifra = chislo % 10;
+			if (cifra % 3 == 0) summa = summa + cifra;
+		}
+	}
+	std::cout << summa;
+	/*for (int item : chisla) {
+		std::cout << item;
+	}*/
+}
+
 void zadanie2() {
 	std::cout << "Введите2" << std::endl;
 	std::string stroka;
@@ -11,7 +34,7 @@ void zadanie2() {
 	std::set<int> sterzh;
 	for (int i = 1; stroka[i - 1] != '\0'; i = i + 2) {
 		kesi[stroka[i] - '0'] = "";
-		sterzh.insert(stroka[i] - '0'); //archipova shluha = whore is True
+		sterzh.insert(stroka[i] - '0');
 
 	}
 	for (int i = 1; stroka[i-1] != '\0'; i = i + 2) {
@@ -40,9 +63,8 @@ void zadanie2() {
 
 }
 
-int main() {
-	setlocale(LC_ALL, "RUSSIAN");
-	/*std::vector<int> Birds;
+void zadanie1() {
+	std::vector<int> Birds;
 	std::cout << "Введите1" << std::endl;
 	for (int over_stns;1;over_stns = 0){
 		std::cin >> over_stns;
@@ -65,7 +87,26 @@ int main() {
 	}
 	for (int stone : res) {
 		std::cout << stone << " ";
-	}*/
-	zadanie2();
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "RUSSIAN");
+	std::cout << "Какое задание решить хочешь" << std::endl;
+	int zdn = 0;
+	std::cin >> zdn;
+	switch (zdn) {
+	case 1:
+		zadanie1();
+		break;
+	case 2:
+		zadanie2();
+		break;
+	case 3:
+		zadanie3();
+		break;
+	default:
+		break;
+	}
  	return 0;
 }
