2e58fc2b6cb46b3e42b26b08294d86ae7f4e2cc0
Игорь Батталов <kakamakaka03@yandex.ru>
2024-03-29 22:52:05 +0700

    рефакторинг кода на C++

--- c_plusplus.cpp
+++ c_plusplus.cpp
@@ -3,75 +3,62 @@
 #include <set>
 #include <map>
 
-void zadanie3() {
-	std::cout << "Введите3" << std::endl;
+void NumberThree() {
+	std::cout << "Введите количество чисел" << std::endl;
 	int n = 0;
 	std::cin >> n;
-	std::vector<int> chisla;
+	std::cout << "Введите числа" << std::endl;
+	std::vector<int> Numbers;
 	for (int i = 0; i < n; i++) {
 		int n1 = 0;
 		std::cin >> n1;
-		chisla.push_back(n1);
+		Numbers.push_back(n1);
 	}
-	int summa = 0;
-	for (int chislo : chisla) {
-		for (int cifra = 0; chislo > 0;chislo = chislo / 10) {
-			cifra = chislo % 10;
-			if (cifra % 3 == 0) summa = summa + cifra;
+	int Summ = 0;
+	for (int Number : Numbers) {
+		for (int Figure = 0;Number > 0;Number = Number / 10) {
+			Figure = Number % 10;
+			if (Figure % 3 == 0) Summ = Summ + Figure;
 		}
 	}
-	std::cout << summa;
-	/*for (int item : chisla) {
-		std::cout << item;
-	}*/
+	std::cout << Summ;
 }
 
-void zadanie2() {
+void NumberTwo() {
 	std::cout << "Введите2" << std::endl;
-	std::string stroka;
-	std::cin >> stroka;
-	std::map<int, std::string> kesi;
-	std::set<int> sterzh;
-	for (int i = 1; stroka[i - 1] != '\0'; i = i + 2) {
-		kesi[stroka[i] - '0'] = "";
-		sterzh.insert(stroka[i] - '0');
+	std::string TextLine;
+	std::cin >> TextLine;
+	std::map<int, std::string> ColorsRings;
+	std::set<int> Kernels;
+	for (int i = 1; TextLine[i - 1] != '\0'; i = i + 2) {
+		ColorsRings[TextLine[i] - '0'] = "";
+		Kernels.insert(TextLine[i] - '0');
 
 	}
-	for (int i = 1; stroka[i-1] != '\0'; i = i + 2) {
-		kesi[stroka[i] - '0'] = kesi[stroka[i] - '0'] + stroka[i-1];
+	for (int i = 1; TextLine[i-1] != '\0'; i = i + 2) {
+		ColorsRings[TextLine[i] - '0'] = ColorsRings[TextLine[i] - '0'] + TextLine[i-1];
 	}
 	for (int i = 0; i < 3;i++) {
-		std::cout << kesi[i] << " ";
+		std::cout << ColorsRings[i] << " ";
 	}
-	int cout = 0;
-	for (int sterzhen: sterzh){
-		if (kesi[sterzhen].find('R') != std::string::npos and kesi[sterzhen].find('W') != std::string::npos and kesi[sterzhen].find('B') != std::string::npos) cout += 1;
+	int count = 0;
+	for (int Kernel: Kernels){
+		if (ColorsRings[Kernel].find('R') != std::string::npos and ColorsRings[Kernel].find('W') != std::string::npos and ColorsRings[Kernel].find('B') != std::string::npos) count += 1;
 	}
-	std::cout << " " << cout;
-	/*for (int i = 0;stroka[i] != '\0'; i =i +2) {
-		char first;
-		first = stroka[i];
-		int second;
-		second = stroka[i+1] - '0';
-		sterz.push_back({first, second});
-		
-	}
-	for (std::pair<char, int> test : sterz) {
-		std::cout << test.first << " " << test.second;
-	}
-*/
-
+	std::cout << " " << count;
 }
 
-void zadanie1() {
+void NumberOne() {
 	std::vector<int> Birds;
-	std::cout << "Введите1" << std::endl;
-	for (int over_stns;1;over_stns = 0){
-		std::cin >> over_stns;
-		if (over_stns == 0) break;
-		Birds.push_back(over_stns);
+	std::cout << "Введите дальность полета каждой птицы, чтобы перейти к следющему шагу введите 0" << std::endl;
+	for (int FlightRange;1; FlightRange = 0){
+		std::cin >> FlightRange;
+		if (FlightRange == 0) break;
+		Birds.push_back(FlightRange);
 	}
-	int stones = 10;
+	std::cout << "Введите количество валунов" << std::endl;
+	int stones = 0;
+	std::cin >> stones;
 	std::vector<int> result;
 	for (int i = 1;i <= stones;i++) {
 		result.push_back(i);
@@ -80,8 +67,8 @@ void zadanie1() {
 	int flag = 0;
 	for (int stone: result) {
 		flag = 0;
-		for (int ptica : Birds) {
-			if (stone % ptica == 0) flag = 1;
+		for (int Bird : Birds) {
+			if (stone % Bird == 0) flag = 1;
 		}
 		if (flag == 0) res.insert(stone);
 	}
@@ -92,18 +79,18 @@ void zadanie1() {
 
 int main() {
 	setlocale(LC_ALL, "RUSSIAN");
-	std::cout << "Какое задание решить хочешь" << std::endl;
-	int zdn = 0;
-	std::cin >> zdn;
-	switch (zdn) {
+	std::cout << "Выберите номер задания" << std::endl;
+	int TaskNumber = 0;
+	std::cin >> TaskNumber;
+	switch (TaskNumber) {
 	case 1:
-		zadanie1();
+		NumberOne();
 		break;
 	case 2:
-		zadanie2();
+		NumberTwo();
 		break;
 	case 3:
-		zadanie3();
+		NumberThree();
 		break;
 	default:
 		break;
