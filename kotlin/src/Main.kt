2b2f31e9ccdef2592ae3754c4daba858dbbaec02
Игорь Батталов <kakamakaka03@yandex.ru>
2024-03-31 13:54:31 +0700

    Добавлена 3 задача на kotlin

--- kotlin/src/Main.kt
+++ kotlin/src/Main.kt
@@ -1,6 +1,6 @@
 import java.util.*
 import kotlin.collections.HashSet
-
+import java.util.Vector
 fun SumNumDivThree(Number: Int): Int{
     var Sum: Int = 0
     var Num: Int = Number
@@ -31,12 +31,11 @@ fun NumberTwo() {
     val ColorsRings = HashMap<Int, String>()
     val Kernels = HashSet<Int>()
     for (i in 1..TextLine.length step 2) {
-        ColorsRings[TextLine[i] - '0'] = "";
+        ColorsRings[TextLine[i] - '0'] = ""
         Kernels.add(TextLine[i] - '0')
-
     }
     for (i in 1..TextLine.length step 2) {
-        ColorsRings[TextLine[i] - '0'] = ColorsRings[TextLine[i] - '0'] + TextLine[i-1];
+        ColorsRings[TextLine[i] - '0'] = ColorsRings[TextLine[i] - '0'] + TextLine[i-1]
     }
     var count: Int = 0
     for (Kernel in Kernels){
@@ -44,7 +43,39 @@ fun NumberTwo() {
     }
     println("$count")
 }
+fun NumberOne() {
+    val scan = Scanner(System.`in`)
+    var Birds = Vector<Int>()
+    println("Введите дальность полета каждой птицы, чтобы перейти к следющему шагу введите 0")
+    while (true){
+        var FlightRange: Int = scan.nextInt()
+        if (FlightRange == 0) break
+        Birds.add(FlightRange)
+        FlightRange = 0
+    }
+    println("Введите количество валунов")
+    var AmountOfStones: Int = scan.nextInt()
+    var Stones = Vector<Int>()
+    for(i in 1..AmountOfStones){
+        Stones.add(i);
+    }
+    val Result = HashSet<Int>()
+    var IsLndngOnAllStone: Int = 0
+    for (Stone in Stones) {
+        var IsLndngOnStone: Int = 0
+        for (Bird in Birds) {
+        if (Bird == 1) IsLndngOnAllStone = 1
+        if (Stone % Bird == 0) IsLndngOnStone = 1
+    }
+        if (IsLndngOnStone == 0) Result.add(Stone)
+    }
+    for (Stone in Result) {
+        println("$Stone ")
+    }
+    if (IsLndngOnAllStone == 1) println("Птицы сядут на каждый камень")
+}
 fun main(){
     //NumberThree()
-    NumberTwo()
+    //NumberTwo()
+    NumberOne()
 }
